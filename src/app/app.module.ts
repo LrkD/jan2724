bbafa30f6aaf8faddcef0b3eee917c95ddc5d6bf
LuisMiraran <luisaldrin.miraran@letran.edu.ph>
2024-01-27 15:34:36 +0800

    DATA BINDING - KARL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
+// app.module.ts
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';  // Correct import path for FormsModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WarningComponent } from './warning/warning.component';
 import { SuccessComponent } from './success/success.component';
-import { ServerComponent } from './server/server.component';
+import { ServerComponent } from './server/server.component'; // Ensure the correct path
 
 @NgModule({
   declarations: [
@@ -16,7 +19,8 @@ import { ServerComponent } from './server/server.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule // Import FormsModule here
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,19 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrl: '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+  styleUrls: ['../../../node_modules/bootstrap/dist/css/bootstrap.min.css']
 })
 export class ServerComponent {
   title = "MS Server";
+  username = ""; // Add a new property for username
 
-  GetData(){
+  GetData() {
     return "Karl";
   }
-  
-  ChangeTitle(event){
-    this.title = "Oracle DB";
-  }
-  constructor(){  }
 
-  ngOnInit(): void{}
+  ChangeTitle(event) {
+    this.title = " ";
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {}
 }
